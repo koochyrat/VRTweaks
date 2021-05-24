f6e71311d470ba23f26e99ce471df12e9e62d53f
EthanFischer <ethan.f.fischer@gmail.com>
2021-05-23 21:38:12 -0500

    Fix pause menu

---
+++ VRTweaks/GamepadGazeCursorFixer.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+
+namespace VRTweaks
+{
+    [HarmonyPatch(typeof(IngameMenu), "UpdateRaycasterStatus")]
+    public static class GamepadGazeCursorFixer
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(ref uGUI_GraphicRaycaster raycaster, IngameMenu __instance)
+        {
+            if (GameInput.IsPrimaryDeviceGamepad() && !VROptions.GetUseGazeBasedCursor())
+            {
+                raycaster.enabled = false;
+            }
+            else
+            {
+                raycaster.enabled = __instance.focused;
+            }
+
+            return false;
+        }
+    }
+}
