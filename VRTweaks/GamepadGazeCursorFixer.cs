6841c8203554c171182d85ae186117f5386c8c29
EthanFischer <ethan.f.fischer@gmail.com>
2021-05-23 21:46:12 -0500

    Fix PDA menu and WIP gamepadinputmodule

--- VRTweaks/GamepadGazeCursorFixer.cs
+++ VRTweaks/GamepadGazeCursorFixer.cs
@@ -3,7 +3,7 @@
 namespace VRTweaks
 {
     [HarmonyPatch(typeof(IngameMenu), "UpdateRaycasterStatus")]
-    public static class GamepadGazeCursorFixer
+    public static class PauseMenu
     {
         [HarmonyPrefix]
         public static bool Prefix(ref uGUI_GraphicRaycaster raycaster, IngameMenu __instance)
@@ -20,4 +20,57 @@ namespace VRTweaks
             return false;
         }
     }
+
+    [HarmonyPatch(typeof(uGUI_PDA), "UpdateRaycasterStatus")]
+    public static class PDAMenu
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(ref uGUI_GraphicRaycaster raycaster, IngameMenu __instance)
+        {
+            if (GameInput.IsPrimaryDeviceGamepad() && !VROptions.GetUseGazeBasedCursor())
+            {
+                raycaster.enabled = false;
+            }
+            else
+            {
+                raycaster.enabled = __instance.focused;
+            }
+
+            return false;
+        }
+    }
+
+    //TODO path the original method in GamepadInputModule with the contents below
+	private void LateUpdate()
+	{
+		this.isControlling = false;
+		bool flag = this.usingController;
+		this.usingController = GameInput.GetPrimaryDevice() == GameInput.Device.Controller;
+		if (!this.usingController)
+		{
+			if (flag && this.currentNavigableGrid != null)
+			{
+				this.currentNavigableGrid.DeselectItem();
+			}
+			return;
+		}
+		if (!flag)
+		{
+			this.OnGroupChanged(this.currentGroup);
+		}
+		if (this.skipOneInputFrame)
+		{
+			this.skipOneInputFrame = false;
+			return;
+		}
+		if (this.IsInputAllowed() && this.currentGroup != null)
+		{
+			this.isControlling = true;
+			this.ProcessInput();
+		}
+		if (Inventory.main)
+		{
+			Inventory.main.UpdateContainers();
+		}
+	}
 }
