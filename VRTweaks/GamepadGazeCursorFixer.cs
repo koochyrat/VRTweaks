b11d0609855f73078a5938748d919870d4d9443a
EthanFischer <ethan.f.fischer@gmail.com>
2021-05-24 21:59:07 -0500

    Some fixes

--- VRTweaks/GamepadGazeCursorFixer.cs
+++ VRTweaks/GamepadGazeCursorFixer.cs
@@ -25,7 +25,7 @@ namespace VRTweaks
     public static class PDAMenu
     {
         [HarmonyPrefix]
-        public static bool Prefix(ref uGUI_GraphicRaycaster raycaster, IngameMenu __instance)
+        public static bool Prefix(ref uGUI_GraphicRaycaster raycaster, uGUI_PDA __instance)
         {
             if (GameInput.IsPrimaryDeviceGamepad() && !VROptions.GetUseGazeBasedCursor())
             {
@@ -40,37 +40,40 @@ namespace VRTweaks
         }
     }
 
-    //TODO path the original method in GamepadInputModule with the contents below
-	private void LateUpdate()
-	{
-		this.isControlling = false;
-		bool flag = this.usingController;
-		this.usingController = GameInput.GetPrimaryDevice() == GameInput.Device.Controller;
-		if (!this.usingController)
-		{
-			if (flag && this.currentNavigableGrid != null)
-			{
-				this.currentNavigableGrid.DeselectItem();
-			}
-			return;
-		}
-		if (!flag)
-		{
-			this.OnGroupChanged(this.currentGroup);
-		}
-		if (this.skipOneInputFrame)
-		{
-			this.skipOneInputFrame = false;
-			return;
-		}
-		if (this.IsInputAllowed() && this.currentGroup != null)
-		{
-			this.isControlling = true;
-			this.ProcessInput();
-		}
-		if (Inventory.main)
-		{
-			Inventory.main.UpdateContainers();
-		}
-	}
+
+    //TODO fix crafting menu by editing GamepadInputModule
+//private void LateUpdate()
+//	{
+//		this.isControlling = false;
+//		bool flag = this.usingController;
+//THIS IS THE PROBLEM!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//		this.usingController = (!VROptions.GetUseGazeBasedCursor() && GameInput.GetPrimaryDevice() == GameInput.Device.Controller);
+//THIS IS THE PROBLEM!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//		if (!this.usingController)
+//		{
+//			if (flag && this.currentNavigableGrid != null)
+//			{
+//				this.currentNavigableGrid.DeselectItem();
+//			}
+//			return;
+//		}
+//		if (!flag)
+//		{
+//			this.OnGroupChanged(this.currentGroup);
+//		}
+//		if (this.skipOneInputFrame)
+//		{
+//			this.skipOneInputFrame = false;
+//			return;
+//		}
+//		if (this.IsInputAllowed() && this.currentGroup != null)
+//		{
+//			this.isControlling = true;
+//			this.ProcessInput();
+//		}
+//		if (Inventory.main)
+//		{
+//			Inventory.main.UpdateContainers();
+//		}
+//	}
 }
